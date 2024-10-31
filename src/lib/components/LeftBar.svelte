936cea0682fc575094985650afd878b97a2ec7da
Abdul Moid Khan Mohammed <skhan852000@gmail.com>
2024-10-31 05:33:15 -0500

    rate profs

--- src/lib/components/LeftBar.svelte
+++ src/lib/components/LeftBar.svelte
@@ -79,7 +79,7 @@
 			</svg>
 		</div>
 		<div class="flex gap-2 hover:text-blue cursor-pointer">
-			<p>Rate Professor</p>
+			<p>Rate Profs.</p>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				viewBox="0 0 24 24"
